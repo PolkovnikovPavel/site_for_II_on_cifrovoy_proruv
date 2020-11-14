5aa307b4e85cbfd9c5140667466c37e6c67a0dee
Павел <pavelpolkovnikov334@gmail.com>
2020-11-14 19:12:52 +0700

    data entry into the site has been created

--- main.py
+++ main.py
@@ -17,7 +17,30 @@ def main():
 
 @app.route("/")
 def index():   # главная страница
-    return render_template("Main.html")
+    data = {}
+    data['top_1_1'] = 'Management Analysts (100000)'
+    data['top_1_2'] = '1Management Analysts (100000)'
+    data['top_1_3'] = 'M1anagement Analysts (100000)'
+    data['top_1_4'] = 'Ma1nagement Analysts (100000)'
+    data['top_1_5'] = 'Management Analysts (100000)'
+    data['top_1_6'] = 'Ma1nagement Analysts (100000)'
+    data['top_1_7'] = 'Ma11nagement Analysts (100000)'
+    data['top_1_8'] = 'Ma1nagement Analysts (100000)'
+    data['top_1_9'] = 'Management Analysts (100000)'
+    data['top_1_10'] = 'M1anagement Analysts (100000)'
+
+    data['top_2_1'] = 'Manag2ement Analysts (100000)'
+    data['top_2_2'] = 'Management Analysts (100000)'
+    data['top_2_3'] = 'Mana2gement Analysts (100000)'
+    data['top_2_4'] = 'Management Analysts (100000)'
+    data['top_2_5'] = 'Man2agement Analysts (100000)'
+    data['top_2_6'] = 'Management Analysts (100000)'
+    data['top_2_7'] = 'Mana22gement Analysts (100000)'
+    data['top_2_8'] = 'Mana2gement Analysts (100000)'
+    data['top_2_9'] = 'Man2agement Analysts (100000)'
+    data['top_2_10'] = 'Ma2nagement Analysts (100000)'
+
+    return render_template("Main.html", **data)
 
 
 if __name__ == '__main__':
